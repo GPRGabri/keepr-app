273cce8ca94c887eb113376e40134fe5363696d1
Gabriel <gprgabrielribeiro@gmail.com>
2023-01-18 18:00:20 +0000

    Implemented props and the contructor from array using map func

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,11 +2,18 @@ const React = require("react");
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from"../notes";
 
 function App(){
     return <div>
     <Header />
-    <Note />
+    <div>{notes.map(note => 
+    <Note 
+        key = {note.key}
+        title = {note.title}
+        content ={note.content}
+    />
+    )}</div>
     <Footer />
     </div>
     }
